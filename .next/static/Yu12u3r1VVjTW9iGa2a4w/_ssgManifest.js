602a5bade21c15e2f4c1604b1b65bbfaddd28820
cactistock <cactistockfiles@gmail.com>
2026-04-18 21:26:24 +0700

    fix cache

---
+++ .next/static/Yu12u3r1VVjTW9iGa2a4w/_ssgManifest.js
@@ -0,0 +1,1 @@
+self.__SSG_MANIFEST=new Set(["\u002F[locale]\u002Fblog\u002F[id]","\u002F[locale]\u002Fcatalogue\u002F[id]","\u002F[locale]\u002Fnews\u002F[id]"]);self.__SSG_MANIFEST_CB&&self.__SSG_MANIFEST_CB()
